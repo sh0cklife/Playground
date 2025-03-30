d560e8dc4df87e83010870246bf6967102000f35
Denis Atanassov <dennis.atanasoff@gmail.com>
2025-03-30 17:29:51 +0300

    dropdown test feat added

--- BPB Online/obj/Debug/net8.0/BPB Online.AssemblyInfo.cs
+++ BPB Online/obj/Debug/net8.0/BPB Online.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BPB Online")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8e44ab35a53243cb1546e0c1a2282523afb7ba9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0893bce24df063c15197ffb8831d7af8baeeffef")]
 [assembly: System.Reflection.AssemblyProductAttribute("BPB Online")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BPB Online")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ DropdownExtraction/Dropdown_Tests.cs
@@ -0,0 +1,17 @@
+namespace DropdownExtraction
+{
+    public class Dropdown_Tests
+
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
